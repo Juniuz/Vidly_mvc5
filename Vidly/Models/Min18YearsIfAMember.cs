1c7949072895a48431385a07da4fe94ac10589fb
Aaron Szczepaniec <altered9th@hotmail.com>
2016-06-10 12:04:21 -0600

    add Client/Server Validation, add Custom Validation, CSRF

---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var cust = (Customer)validationContext.ObjectInstance;
+
+            if (cust.MembershipTypeId == MembershipType.Unknown || 
+                cust.MembershipTypeId == MembershipType.PayAsYouGo)
+                return ValidationResult.Success;
+
+            if (cust.BirthDate == null)
+                return new ValidationResult("Birthdate is required.");
+
+            var age = DateTime.Today.Year - cust.BirthDate.Value.Year;
+
+            return (age >= 18) 
+                ? ValidationResult.Success 
+                : new ValidationResult("Must be at least 18 years old for chosen Membership type.");
+        }
+    }
+}
