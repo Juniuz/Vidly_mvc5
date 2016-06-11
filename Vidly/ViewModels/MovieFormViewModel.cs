f84725ce49db8c34f7057f4ad0607f552f934614
Aaron Szczepaniec <altered9th@hotmail.com>
2016-06-11 11:32:07 -0600

    Exercise 5 - add Client/Server Validation to Movie Form, refactor MovieFormViewModel

--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -9,7 +9,47 @@ namespace Vidly.ViewModels
 {
     public class MovieFormViewModel
     {
+        public MovieFormViewModel()
+        {
+            Id = 0;
+        }
+
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            GenreId = movie.GenreId;
+            ReleaseDate = movie.ReleaseDate;
+            NumberInStock = movie.NumberInStock;
+        }
+
         public IEnumerable<Genre> Genres { get; set; }
-        public Movie Movie { get; set; }
+
+        public int? Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public byte? GenreId { get; set; }
+
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        [Display(Name = "Number in Stock")]
+        public byte? NumberInStock { get; set; }
+
+        public string Title
+        {
+            get
+            {
+                return Id == 0 ? "New Movie" : "Edit Movie";
+            }
+        }
     }
 }
