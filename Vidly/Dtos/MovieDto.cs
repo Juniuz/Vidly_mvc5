c868a4673fa9bc4ca30c7120b9cba0070c7cb7fb
Aaron Szczepaniec <altered9th@hotmail.com>
2016-06-16 23:11:06 -0600

    Exercise 6 - add Movies API

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        [Display(Name = "Number in Stock")]
+        public byte NumberInStock { get; set; }
+    }
+}
